5e78c5207794203917623cfc3fd71c5112b99e0b
Prabhatkumar14 <ankitsingh03737@gmail.com>
2026-05-08 00:44:19 +0530

    final fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Backend URL
-const API_URL = 'https://seekrx-backend.onrender.com';
+const API_URL = 'https://seekrx-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
